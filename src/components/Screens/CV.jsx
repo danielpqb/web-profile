7d006db634a60e7c0318210953bbdbd42038d439
Daniel <danielpqb@gmail.com>
2022-08-16 15:11:12 -0300

    CV update

--- src/components/Screens/CV.jsx
+++ src/components/Screens/CV.jsx
@@ -11,182 +11,172 @@ export default function CV() {
     {
       src: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/javascript-colored.svg",
       title: "JavaScript",
-      type: 'main',
+      type: 'high',
     },
     {
       src: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/typescript-colored.svg",
       title: "TypeScript",
-      type: 'main',
+      type: 'high',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
       title: "React",
-      type: 'main',
+      type: 'high',
     },
     {
       src: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/html5-colored.svg",
       title: "HTML5",
-      type: 'main',
+      type: 'high',
     },
     {
       src: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/css3-colored.svg",
       title: "CSS3",
-      type: 'main',
+      type: 'high',
     },
     {
       src: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/nodejs-colored.svg",
       title: "NodeJS",
-      type: 'other',
+      type: 'high',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
       title: "MongoDB",
-      type: 'other',
+      type: 'high',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg",
       title: "C#",
-      type: 'other',
+      type: 'secondary',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg",
       title: "C++",
-      type: 'other',
+      type: 'secondary',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
       title: "Python",
-      type: 'other',
+      type: 'secondary',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg",
       title: "MySQL",
-      type: 'other',
+      type: 'secondary',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/jupyter/jupyter-original-wordmark.svg",
-      title: "Jupyter",
-      type: 'other',
+      title: "JupyterNotebook",
+      type: 'secondary',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/microsoftsqlserver/microsoftsqlserver-plain.svg",
       title: "SQLServer",
-      type: 'other',
+      type: 'high',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/dot-net/dot-net-original.svg",
       title: ".NET",
-      type: 'other',
+      type: 'secondary',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/azure/azure-original.svg",
       title: "Azure",
-      type: 'other',
+      type: 'secondary',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/visualstudio/visualstudio-plain.svg",
       title: "VisualStudio",
-      type: 'other',
+      type: 'secondary',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
       title: "VSCode",
-      type: 'other',
+      type: 'secondary',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original.svg",
       title: "PostgreSQL",
-      type: 'other',
+      type: 'high',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/amazonwebservices/amazonwebservices-original.svg",
       title: "AWS",
-      type: 'other',
+      type: 'high',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original.svg",
       title: "Docker",
-      type: 'other',
+      type: 'high',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
       title: "Git",
-      type: 'other',
+      type: 'secondary',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/anaconda/anaconda-original.svg",
       title: "Anaconda",
-      type: 'other',
+      type: 'secondary',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/eslint/eslint-original.svg",
       title: "ESLint",
-      type: 'other',
+      type: 'secondary',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original.svg",
       title: "Express",
-      type: 'other',
+      type: 'secondary',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/heroku/heroku-original.svg",
       title: "Heroku",
-      type: 'other',
+      type: 'secondary',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/ionic/ionic-original.svg",
       title: "Ionic",
-      type: 'other',
+      type: 'secondary',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg",
       title: "NextJS",
-      type: 'other',
-    },
-    {
-      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
-      title: "NodeJS",
-      type: 'other',
+      type: 'secondary',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/npm/npm-original-wordmark.svg",
       title: "npm",
-      type: 'other',
+      type: 'secondary',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/numpy/numpy-original.svg",
       title: "Numpy",
-      type: 'other',
+      type: 'secondary',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/pandas/pandas-original.svg",
       title: "Pandas",
-      type: 'other',
+      type: 'secondary',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/photoshop/photoshop-line.svg",
       title: "Photoshop",
-      type: 'other',
-    },
-    {
-      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
-      title: "Python",
-      type: 'other',
+      type: 'secondary',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/trello/trello-plain.svg",
       title: "Trello",
-      type: 'other',
+      type: 'secondary',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/unity/unity-original.svg",
       title: "Unity",
-      type: 'other',
+      type: 'secondary',
     },
     {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/unrealengine/unrealengine-original.svg",
       title: "UE5",
-      type: 'other',
+      type: 'secondary',
     },
   ]
 
@@ -211,32 +201,26 @@ export default function CV() {
       <Text>
         <h1>Estúdio OBJ<em>RJ, Brazil</em></h1>
         <h2>Production Analyst</h2>
-        <h3>• Product traceability with the creation and implementation of documents and verification tools.</h3>
-        <h3>• Discrimination of the production stages, and elaboration of the quality control of the products by process.</h3>
-        <h3>• Installation of methods for reusing unfinished or skewed parts.</h3>
-        <h3>• Development of technological solutions for inventory control and management, purchases of materials and equipment, and production planning and control.</h3>
       </Text>
       <br />
 
       <SectionBreaker>Courses</SectionBreaker>
       <Text>
+        <h4>Full Stack Web Development<em>Driven</em></h4><br />
         <h4>BI and Data Warehouse with SQL Server and Power BI<em>Alura</em></h4><br />
-        <h4>Python for Data Science<em>Alura</em></h4><br />
-        <h4>Python, C++, SQL and Java<em>Udemy</em></h4><br />
-        <h4>Fixed Incomes (Treasury bonds, CD and debentures)<em>XPI</em></h4><br />
         <h4>Master I (English proficiency)<em>Cultura <br /> Inglesa</em></h4>
       </Text>
 
-      <SectionBreaker>Main Skills</SectionBreaker>
+      <SectionBreaker>High Level Skills</SectionBreaker>
       <Skills>
-        {skills.filter((value) => (value.type === 'main')).map((value, index) => {
+        {skills.filter((value) => (value.type === 'high')).map((value, index) => {
           return <Skill height={90} skill={value} alt="" key={index} />
         })}
       </Skills>
 
-      <SectionBreaker>Other Skills</SectionBreaker>
+      <SectionBreaker>Secondary Skills</SectionBreaker>
       <Skills>
-        {skills.filter((value) => (value.type === 'other')).map((value, index) => {
+        {skills.filter((value) => (value.type !== 'high')).map((value, index) => {
           return <Skill height={90} skill={value} alt="" key={index} />
         })}
       </Skills>
